3c673ec9abff13b6cababe26126ecddfca4b9037
BrianLeabold <livingsingl@hotmail.com>
2020-01-04 19:25:39 -0800

    uploading multiple images to update tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,6 @@
+const multer = require('multer');
+const sharp = require('sharp');
+// const fs = require('fs');
 const Tour = require('./../models/tourModel');
 const factory = require('./handlerFactory');
 const catchAsync = require('./../utils/catchAsync');
@@ -172,4 +175,58 @@ exports.monthlyPlan = catchAsync(async (req, res, next) => {
       plan
     }
   });
+});
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(
+      new AppError('This is not an image, please only upload images.', 400),
+      false
+    );
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 }
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
 });
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -49,6 +49,8 @@ router
   .patch(
     authController.protect,
     authController.restrictTo('admin'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.updateTour
   )
   .delete(
