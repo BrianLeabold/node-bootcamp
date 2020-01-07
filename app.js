49f1aaeb43563de3e9629ef5f916a9fbbc267910
BrianLeabold <livingsingl@hotmail.com>
2020-01-07 08:49:08 -0800

    added cors middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const gobalErrorHandler = require('./controllers/errorController');
 
@@ -24,6 +25,10 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 // 1) Global Middleware
+// Implement CORS
+app.use(cors());
+// app.options('/api/v1/tours/:id', cors());
+app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP Headers
 app.use(helmet());
